71f65a674ae351ad91aa993417bb3c758b02034c
Raunak Sett <sett.raunak@gmail.com>
2024-06-06 12:34:42 +0400

    feat: added go router

---
+++ lib/router/app_router.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_starter/home/home.dart';
+import 'package:go_router/go_router.dart';
+
+class AppRouter {
+  // Runs the authentication middle ware
+  String? _runAuthMiddleware(context, state) {
+    return null;
+  }
+
+  GoRouter getRouter() {
+    return GoRouter(
+      routes: [
+        GoRoute(
+          path: '/',
+          redirect: _runAuthMiddleware,
+          builder: (context, state) {
+            return const HomePage();
+          },
+        ),
+      ],
+    );
+  }
+}
